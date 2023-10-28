e5fa7ebd2cf87409d160e63ec3701e64c5b6658a
Ido Don <76511079+Ido-Don@users.noreply.github.com>
2023-10-29 00:35:00 +0300

    initial commit

---
+++ device_executer.py
@@ -0,0 +1,90 @@
+import json
+from typing import List
+import netmiko
+
+
+def enter_enable_mode(callback):
+    def enable(device: netmiko.BaseConnection):
+        device.enable()
+        callback(device)
+
+    return enable
+
+
+def enter_configure_terminal_mode(callback):
+    def configure_terminal(device: netmiko.BaseConnection):
+        device.enable()
+        device.config_mode()
+        callback(device)
+
+    return configure_terminal
+
+
+def enter_user_mode(callback):
+    def user(device: netmiko.BaseConnection):
+        device.exit_enable_mode()
+        callback(device)
+
+    return user
+
+
+def execute_script(command_file_path, device_file_path, output_file_path, permission_level="user"):
+    commands = commands_reader(command_file_path)
+    device_options = device_reader(device_file_path)
+    write_output = handle_result(output_file_path)
+    execute = execute_commands(commands, write_output)
+    permission_escalation = escalate_permission(execute, permission_level)
+    connect(device_options, permission_escalation)
+
+
+def escalate_permission(execute, permissions):
+    permission_escalation = None
+    if permissions == "enable":
+        permission_escalation = enter_enable_mode(execute)
+    if permissions == "configure terminal":
+        permission_escalation = enter_configure_terminal_mode(execute)
+    if permissions == "user":
+        permission_escalation = enter_user_mode(execute)
+    return permission_escalation
+
+
+def is_valid_command(command: str):
+    if command:
+        return True
+    return False
+
+
+def handle_result(output_file_path: str):
+    def write_result(result):
+        with open(output_file_path, 'w') as output_file:
+            output_file.write(result)
+
+    return write_result
+
+
+def execute_commands(commands: List[str], result_callback):
+    def execute(device: netmiko.BaseConnection):
+        result = device.send_config_set(commands)
+        result_callback(result)
+    return execute
+
+
+def connect(device_options, callback):
+    device = netmiko.ConnectHandler(**device_options)
+    callback(device)
+    device.disconnect()
+
+
+def device_reader(device_file_path):
+    with open(device_file_path) as device_file:
+        device_options = json.load(device_file)
+    return device_options
+
+
+def commands_reader(command_file_path):
+    with open(command_file_path) as commands_file:
+        commands = commands_file.readlines()
+        commands = [command.strip("\n ") for command in commands]
+        commands = filter(lambda command: is_valid_command(command), commands)
+        commands = list(commands)
+    return commands
---
+++ execute_many.py
@@ -0,0 +1,50 @@
+import argparse
+import os
+
+from device_executer import execute_script
+
+
+def get_arguments():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("-c", "--commands-file",
+                            help="the file path for commands, default is 'commands.txt' under the current working "
+                                 "directory",
+                            default="commands.txt")
+    arg_parser.add_argument("-o", "--output-folder",
+                            help="the folder path for the commands output",
+                            default=r"outputs")
+    arg_parser.add_argument("-d", "--device-folder",
+                            help="the folder for the connection parameters for the devices (in json format)",
+                            default=r"devices")
+
+    arg_parser.add_argument("-p", "--permission-level",
+                            help="execute the commands in which permission level - user, enable or configure terminal",
+                            default="enable", type=str)
+    arguments = arg_parser.parse_args()
+    command_file_path = arguments.commands_file
+    output_folder_path = arguments.output_folder
+    device_folder_path = arguments.device_folder
+    permission_level = arguments.permission_level
+    return command_file_path, device_folder_path, output_folder_path, permission_level
+
+
+def get_file(folder_path):
+    files = os.scandir(folder_path)
+    files = filter(lambda path: os.path.isfile(path), files)
+    return files
+
+
+def main():
+    command_file_path, device_folder_path, output_folder_path, permission_level = get_arguments()
+    device_files = get_file(device_folder_path)
+    if not os.path.isdir(output_folder_path):
+        os.mkdir(output_folder_path)
+
+    for device_file_path in device_files:
+        device_file = os.path.basename(device_file_path)
+        output_file_path = os.path.join(output_folder_path, device_file)
+        execute_script(command_file_path, device_file_path, output_file_path, permission_level)
+
+
+if __name__ == '__main__':
+    main()
