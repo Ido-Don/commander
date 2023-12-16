5046f48991b981c44b62da9a816da33abf76d35a
Ido Don <76511079+Ido-Don@users.noreply.github.com>
2023-12-16 19:46:17 +0200

    added parametrize test

--- test/test_device.py
+++ test/test_device.py
@@ -2,7 +2,7 @@ import os.path
 
 import pytest
 
-from src.device import add_device_entry, does_device_exist
+from src.device import add_device_entry, does_device_exist, DeviceEntry
 from src.init import create_new_keepass_db
 
 KEEPASS_TEST_DB_PATH = r"test.kdbx"
@@ -11,7 +11,6 @@ KEEPASS_TEST_PASSWORD = "T3st"
 
 @pytest.fixture
 def create_new_test_db():
-
     # remove old test db if exist
     if os.path.isfile(KEEPASS_TEST_DB_PATH):
         os.remove(KEEPASS_TEST_DB_PATH)
@@ -20,6 +19,25 @@ def create_new_test_db():
     create_new_keepass_db(KEEPASS_TEST_DB_PATH, KEEPASS_TEST_PASSWORD)
 
 
-def test_insert_device(create_new_test_db):
-    add_device_entry("r1", "", "", {"host": "127.0.0.1"}, KEEPASS_TEST_DB_PATH, KEEPASS_TEST_PASSWORD)
-    assert does_device_exist("r1", KEEPASS_TEST_DB_PATH, KEEPASS_TEST_PASSWORD)
+@pytest.mark.parametrize("device, expectation", [
+    (
+            {
+                "name": "r1",
+                "username": "",
+                "password": "",
+                "device_options": {}
+            },
+            True
+    ), (
+            {
+                "name": "r2",
+                "username": "",
+                "password": "",
+                "device_options": {}
+            }, True
+    )
+])
+def test_insert_device(create_new_test_db, device, expectation):
+    add_device_entry(DeviceEntry(**device), KEEPASS_TEST_DB_PATH, KEEPASS_TEST_PASSWORD)
+    device_name = device["name"]
+    assert does_device_exist(device_name, KEEPASS_TEST_DB_PATH, KEEPASS_TEST_PASSWORD) == expectation
