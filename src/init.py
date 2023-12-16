a9de9b58401f6a4c9b8d58d238373326cd3df29c
Ido Don <76511079+Ido-Don@users.noreply.github.com>
2023-12-16 17:34:43 +0200

    pulled up keepass_path and password

--- src/init.py
+++ src/init.py
@@ -2,22 +2,23 @@ import os
 
 from pykeepass import pykeepass
 
-from src.global_variables import COMMANDER_DIRECTORY, KEEPASS_DB_PATH, KEEPASS_PASSWORD
 
-
-def init_program():
-    if is_initialized():
+def init_program(commander_directory, keepass_db_path, keepass_password):
+    if is_initialized(keepass_db_path, keepass_password):
         return
-    os.mkdir(COMMANDER_DIRECTORY)
-    create_new_keepass_db()
+    if not os.path.isdir(commander_directory):
+        os.mkdir(commander_directory)
+    if not os.path.isfile(keepass_db_path):
+        create_new_keepass_db(keepass_db_path, keepass_password)
 
 
-def create_new_keepass_db():
-    kp = pykeepass.create_database(KEEPASS_DB_PATH, password=KEEPASS_PASSWORD)
+def create_new_keepass_db(keepass_db_path, keepass_password):
+    kp = pykeepass.create_database(keepass_db_path, password=keepass_password)
+    kp.add_group(kp.root_group, "devices")
     kp.save()
 
 
-def is_initialized():
-    if os.path.isdir(COMMANDER_DIRECTORY):
-        return os.path.isfile(KEEPASS_DB_PATH)
+def is_initialized(directory, keepass_db_path):
+    if os.path.isdir(directory):
+        return os.path.isfile(keepass_db_path)
     return False
