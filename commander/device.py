3d7c8cf6070dcac1d8b87027bf29a2d77b02955f
Ido Don <76511079+Ido-Don@users.noreply.github.com>
2024-01-05 15:43:17 +0200

    added docstrings

--- commander/device.py
+++ commander/device.py
@@ -1,8 +1,12 @@
-from typing import Optional
+from typing import Optional, Any, Dict
 from pydantic import BaseModel
 
 
 class Device(BaseModel):
+    """
+    netmiko.ConnectHandler takes in a lot of arguments that change from execution to execution.
+    this class is here to hold the data netmiko.ConnectHandler needs to run.
+    """
     name: str
     username: str
     password: str
@@ -15,16 +19,33 @@ class Device(BaseModel):
         return device_string
 
     def get_ssh_string(self):
+        """
+        this function takes the data in the class and convert it to a valid ssh string.
+        example - Device(host="google", username="root", port="22"...).get_ssh_string() -> root@google:22
+        then you can use it with ssh command to connect to the remote device.
+        example - ssh root@google:22
+        :return: an ssh string (username@{ip/hostname}:port)
+        """
         ssh_string = ""
+
         if self.username:
             ssh_string += f"{self.username}@"
+
         ssh_string += self.host
+
         if self.port:
             ssh_string += f":{self.port}"
+
         return ssh_string
 
     @property
-    def device_options(self):
+    def device_options(self) -> Dict[str, Any]:
+        """
+        netmiko.ConnectionHandler() accepts only arguments.
+        this property convert the data in this class to arguments netmiko.ConnectionHandler() can accept.
+        example - netmiko.ConnectionHandler(**device.device_options).
+        :return: a dictionary containing the arguments netmiko.ConnectionHandler() needs to run
+        """
         json_dump = self.model_dump()
         del json_dump["name"]
         return json_dump
