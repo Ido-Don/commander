6cc841e66d115f4c95b2b9f2075275711c833978
Ido Don <76511079+Ido-Don@users.noreply.github.com>
2024-01-10 23:36:39 +0200

    removed unused function

--- NetworkCommander/device_list.py
+++ NetworkCommander/device_list.py
@@ -5,13 +5,6 @@ import typer
 from NetworkCommander.device import Device
 
 
-def format_device_list(devices: List[Device]) -> str:
-    output = ""
-    for device in devices:
-        output += str(device) + '\n'
-    return output
-
-
 def print_devices(devices: List[Device]):
     if not devices:
         typer.echo("there are 0 devices")
