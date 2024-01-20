9ddc3d2889ac839a8b3b1524062d0e5cf5c354e1
Ido Don <76511079+Ido-Don@users.noreply.github.com>
2024-01-20 22:23:30 +0200

    moved KEEPASS_DB_PATH to KeepassDB

--- NetworkCommander/__init__.py
+++ NetworkCommander/__init__.py
@@ -2,4 +2,3 @@ import os
 __version__ = '0.2.3'
 HOME_FOLDER = os.path.expanduser("~")
 COMMANDER_DIRECTORY = os.path.join(HOME_FOLDER, ".commander")
-KEEPASS_DB_PATH = os.path.join(COMMANDER_DIRECTORY, "db.kdbx")
