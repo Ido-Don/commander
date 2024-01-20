f12a566ce42d489ac737d7f48331a1244c23de1f
Ido Don <76511079+Ido-Don@users.noreply.github.com>
2024-01-20 16:19:59 +0200

    changed the python versions possible and bumped published version

--- NetworkCommander/__init__.py
+++ NetworkCommander/__init__.py
@@ -1,5 +1,5 @@
 import os
-__version__ = '0.2.1'
+__version__ = '0.2.3'
 HOME_FOLDER = os.path.expanduser("~")
 COMMANDER_DIRECTORY = os.path.join(HOME_FOLDER, ".commander")
 KEEPASS_DB_PATH = os.path.join(COMMANDER_DIRECTORY, "db.kdbx")
