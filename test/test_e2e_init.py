336540ec77c3ea72be7fa318444e3d58adecba61
Ido Don <76511079+Ido-Don@users.noreply.github.com>
2024-02-25 22:32:52 +0200

    added the first end-to-end test!

--- test/test_e2e_init.py
+++ test/test_e2e_init.py
@@ -1,8 +1,11 @@
 import os.path
+import os.path
 import shutil
+
 import pytest
 from typer.testing import CliRunner
-from networkcommander.config import HOME_FOLDER
+
+from networkcommander.config import config
 from networkcommander.main import app
 
 
@@ -12,10 +15,20 @@ def runner():
 
 
 def test_frash_install(runner):
-    commander_directory = os.path.join(HOME_FOLDER, ".commander")
-    if os.path.isdir(commander_directory):
-        shutil.rmtree(commander_directory)
+    if os.path.isdir(config["commander_directory"]):
+        shutil.rmtree(config["commander_directory"])
     result = runner.invoke(app, ["init"], input="123")
     assert result.exit_code == 0
-    assert f"creating a new database in {commander_directory}" in result.stdout
+    assert f"creating a new database in {config['commander_directory']}" in result.stdout
+    assert "finished the initialization process, have a great day" in result.stdout
+
+
+def test_reinitialize(runner):
+    if not os.path.isdir(config["commander_directory"]):
+        os.mkdir(config["commander_directory"])
+        open(config["keepass_db_path"])
+    result = runner.invoke(app, ["init"], input="y\n123")
+    assert result.exit_code == 0
+    assert "commander is already initialized" in result.stdout
+    assert f"creating a new database in {config['commander_directory']}" in result.stdout
     assert "finished the initialization process, have a great day" in result.stdout
