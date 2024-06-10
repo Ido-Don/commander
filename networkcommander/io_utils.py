e0c0fe032784f81519cee1d80dea8332e9d7c8a2
Ido Don <76511079+Ido-Don@users.noreply.github.com>
2024-06-11 00:28:39 +0300

    removed some unneeded logging massages

--- networkcommander/io_utils.py
+++ networkcommander/io_utils.py
@@ -60,17 +60,14 @@ def load_user_config():
 
     Note: The configuration file is expected to be in JSON format.
     """
-    print("load config")
     commander_logger.info("loading user config from %s", USER_CONFIG_FILE)
     if os.path.isfile(USER_CONFIG_FILE):
-        commander_logger.debug("starting to open file located at %s", USER_CONFIG_FILE)
         with open(USER_CONFIG_FILE, encoding="UTF-8") as json_file:
-            commander_logger.debug("successfully opened file located at %s", USER_CONFIG_FILE)
+            commander_logger.info("successfully opened file located at %s", USER_CONFIG_FILE)
             file_content = json_file.read()
             if not file_content:
-                commander_logger.debug("file %s had no data", USER_CONFIG_FILE)
+                commander_logger.info("file %s had no data", USER_CONFIG_FILE)
                 return
-            commander_logger.debug("loading json", USER_CONFIG_FILE)
             user_custom_config = json.loads(file_content)
             config.update(user_custom_config)
             commander_logger.info("finished loading user config")
