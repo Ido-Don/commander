a73fe7eb9e0cb8c2eb0966495362290659d2232b
Ido Don <76511079+Ido-Don@users.noreply.github.com>
2024-06-16 11:08:59 +0300

    moved the addition of the file handler to a function

--- networkcommander/commander_logging.py
+++ networkcommander/commander_logging.py
@@ -10,19 +10,19 @@ from networkcommander.config import config
 logging_id = uuid.uuid4()
 
 commander_logger = logging.getLogger(config["logger_name"])
-commander_logger.setLevel(config["logging_file_level"])
-stream_handler = logging.StreamHandler(sys.stdout)
 
 
-stream_formatter = logging.Formatter("%(asctime)s : %(levelname)s : %(name)s : %(message)s")
-stream_handler.setFormatter(stream_formatter)
+def configure_file_handler(path: str):
+    commander_logger.setLevel(config["logging_file_level"])
+    if os.path.isfile(path):
+        file_handler = logging.FileHandler(path)
+        file_formatter = logging.Formatter(f'{logging_id} : %(asctime)s : %(levelname)s : %(name)s : %(message)s')
+        file_handler.setFormatter(file_formatter)
 
-if os.path.isfile(config["log_file_path"]):
-    file_handler = logging.FileHandler(config["log_file_path"])
-    file_formatter = logging.Formatter(f'{logging_id} : %(asctime)s : %(levelname)s : %(name)s : %(message)s')
-    file_handler.setFormatter(file_formatter)
+        commander_logger.addHandler(file_handler)
 
-    commander_logger.addHandler(file_handler)
+
+configure_file_handler(config["log_file_path"])
 
 
 class LogLevel(str, Enum):
@@ -37,7 +37,10 @@ class LogLevel(str, Enum):
         return self.value
 
 
-def add_stream_handler(log_level: LogLevel):
+def add_console_handler(log_level: LogLevel):
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_formatter = logging.Formatter("%(asctime)s : %(levelname)s : %(name)s : %(message)s")
+    stream_handler.setFormatter(stream_formatter)
     stream_handler.setLevel(log_level)
     commander_logger.addHandler(stream_handler)
     commander_logger.debug("added stream logger at log level %s", log_level)
